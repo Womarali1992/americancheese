bff9b27ecd7da0ace56b0212a43e5220214bc85a
omaranything <38318198-omaranything@users.noreply.replit.com>
2025-05-27 12:06:12 +0000

    Restored to '8346e386efb10c860a476fe2491259f2f90313b8'
    Replit-Restored-To: 8346e386efb10c860a476fe2491259f2f90313b8

--- client/src/pages/contacts/index.tsx
+++ client/src/pages/contacts/index.tsx
@@ -1165,40 +1165,22 @@ export default function ContactsPage() {
 
         {/* View Mode Tabs */}
         <Tabs defaultValue="categories">
-          <TabsList className="grid w-full grid-cols-3" style={{ borderColor: 'var(--finishings)' }}>
+          <TabsList className="grid w-full grid-cols-3 border-blue-500">
             <TabsTrigger 
               value="categories" 
-              className="data-[state=active]:text-white"
-              style={{ 
-                backgroundColor: 'transparent',
-                '--active-bg': 'var(--finishings)',
-                '--active-text': 'white'
-              } as React.CSSProperties & { '--active-bg': string; '--active-text': string }}
-              data-active-style="true"
+              className="data-[state=active]:bg-blue-50 data-[state=active]:text-blue-700"
             >
               Category View
             </TabsTrigger>
             <TabsTrigger 
               value="list" 
-              className="data-[state=active]:text-white"
-              style={{ 
-                backgroundColor: 'transparent',
-                '--active-bg': 'var(--finishings)',
-                '--active-text': 'white'
-              } as React.CSSProperties & { '--active-bg': string; '--active-text': string }}
-              data-active-style="true"
+              className="data-[state=active]:bg-blue-50 data-[state=active]:text-blue-700"
             >
               List View
             </TabsTrigger>
             <TabsTrigger 
               value="projects" 
-              className="data-[state=active]:text-white"
-              style={{ 
-                backgroundColor: 'transparent',
-                '--active-bg': 'var(--finishings)',
-                '--active-text': 'white'
-              } as React.CSSProperties & { '--active-bg': string; '--active-text': string }}
-              data-active-style="true"
+              className="data-[state=active]:bg-blue-50 data-[state=active]:text-blue-700"
             >
               Project View
             </TabsTrigger>
